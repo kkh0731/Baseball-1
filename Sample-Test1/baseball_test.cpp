84a92bb352ec4ceb3f5e55e1568eb7a9911aab00
kkh <dhadgfl@gmail.com>
2023-06-02 10:09:28 +0900

    baseballgame commit

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -1,6 +1,37 @@
 #include "pch.h"
 #include "../Day9/baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
+TEST(BaseballGame, InvalidArgument) {
+	Baseball baseball("123");
+	EXPECT_THROW(baseball.guess(""), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1543"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1v3"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("121"), std::invalid_argument);
+}
+
+TEST(BaseballGame, PerfectMatch) {
+	Baseball baseball("123");
+	Result result = baseball.guess("123");
+
+	EXPECT_EQ(true, result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST(BaseballGame, MissMatch) {
+	Baseball baseball("123");
+	Result result = baseball.guess("789");
+
+	EXPECT_EQ(false, result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST(BaseballGame, HalfMatch) {
+	Baseball baseball("123");
+	Result result = baseball.guess("328");
+
+	EXPECT_EQ(false, result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
 }
